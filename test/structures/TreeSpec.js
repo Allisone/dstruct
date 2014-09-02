775ca222503fe29b2010942f6da7538e2789bd35
Chelsea Lura <mail@chelsealura.com>
2014-09-02 13:08:35 -0700

    Added tests to Tree Spec. Two currently fail, with error message noted above in comments

--- test/structures/TreeSpec.js
+++ test/structures/TreeSpec.js
@@ -28,7 +28,7 @@
             });
 
             it("Should remove a key inserted.", function () {
-                 tree.insert(4, 2);
+                tree.insert(4, 2);
                 tree.insert(2, 2);
                 tree.insert(4, 4);
                 tree.remove(4);
@@ -46,6 +46,58 @@
 
                 expect(tree.size() === 1);
             });
+
+            //currently fails -- error: NullPointerException -argument is null
+            it("isEmpty() should return false, even if all values inserted into the tree are null", function () {
+                tree.insert(1, null);
+                tree.insert(2, null);
+                tree.insert(3, null);
+
+                expect(tree.isEmpty() === false);
+                expect(tree.size() === 3);
+            });
+
+            //currently fails -- error: NullPointerException -argument is null
+            it("isEmpty() should return false, even if all values inserted into the tree are undefined", function() {
+                tree.insert(1, undefined);
+                tree.insert(2, undefined);
+                tree.insert(3, undefined);
+
+                expect(tree.isEmpty() === false);
+                expect(tree.size() === 3);
+            });
+
+            it("isEmpty() should return false, even if all values inserted into keys are 0 or negatives", function() {
+                tree.insert(1, 0);
+                tree.insert(2, -1);
+                tree.insert(3, 0);
+
+                expect(tree.isEmpty() === false);
+                expect(tree.size() === 3);
+            });
+
+            it("clear() should remove all items in a tree with multiple items", function () {
+                tree.insert(1, 5);
+                tree.insert(2, 8);
+                tree.insert(3, 0);
+
+                tree.clear();
+
+                expect(tree.isEmpty() === true);
+                expect(tree.size() === 0);
+            });
+
+            it("clear() should remove all items in a tree with multiple items", function () {
+                tree.insert(1, 5);
+                tree.insert(2, 8);
+                tree.insert(3, 0);
+
+                tree.clear();
+
+                expect(tree.isEmpty() === true);
+                expect(tree.size() === 0);
+            });
+
         });
     }
 
